ac7c43cba67a28181b64f208c3cec5ab04858293
Hachem Ouanes <Hachem.Ouanes@gmail.com>
2022-04-01 01:17:24 +0100

    Initialized boilerplate and implemented navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import { Menu, Button, Avatar, Typography } from "antd";
+import icon from '../images/icon.png'
+import { BulbOutlined, FundOutlined, HomeOutlined, MoneyCollectOutlined } from '@ant-design/icons';
+
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div className="logo-container">
+                <Avatar src={icon} size="large" />
+                <Typography.Title level={2} className="logo">
+                    <Link to="/">Cryptoverse</Link>
+                </Typography.Title>
+                <Menu theme="dark">
+                    <Menu.Item icon={<HomeOutlined />}>
+                        <Link to="/">Home</Link>
+                    </Menu.Item>
+                    <Menu.Item icon={<FundOutlined />}>
+                        <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+                    </Menu.Item>
+                    <Menu.Item icon={<MoneyCollectOutlined />}>
+                        <Link to="/exchanges">Exchanges</Link>
+                    </Menu.Item>
+                    <Menu.Item icon={<BulbOutlined />}>
+                        <Link to="/news">News</Link>
+                    </Menu.Item>
+                </Menu>
+            </div>
+
+        </div>
+    );
+};
+
+export default Navbar;
